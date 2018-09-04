f032a8b1701de708f58812af345059948991174c
KONRADSPC\khafe <konrad.hafen@gmail.com>
2018-09-04 11:57:57 -0700

    adding functions for precip, snowmelt, and basic stats

--- hydmod/precip.py
+++ hydmod/precip.py
@@ -1,7 +1,22 @@
 import numpy as np
 import os
 
-def meltDegreeDay_USACE(k, tbase, tmean):
-    melt = k*(tmean-tbase)
+def meltDegreeDay_USACE(k, temp, tbase=0.0):
+    """
+    Degree day snowmelt with US Army Corps of Engineers empirical model
+
+    Args:
+        k: Empirically derived constant with units length/degree/day
+        temp: Daily mean (or min or max) air temperature (numpy array)
+        tbase: Base temperature. USACE recommends 0.0 (C) for forested areas and -4.4 (C) for open areas (default = 0.0)
+
+    Returns:
+        numpy array of daily melt values
+
+    """
+    melt = k*(temp-tbase)
     melt[melt < 0] = 0
     return melt
+
+def precipAsSnow(precip, temp, train=3.0, tsnow=0.0):
+    return 0
