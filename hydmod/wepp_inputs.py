9281e7e1e3a116a437d4b0756c673d23a49a80af
Konrad Hafen <konrad.hafen@gmail.com>
2018-11-03 14:21:28 -0700

    smr with wepp inputs

--- hydmod/wepp_inputs.py
+++ hydmod/wepp_inputs.py
@@ -51,8 +51,86 @@ tsnow = 0.
 #calculate melted snow
 swe_melt2d = np.apply_along_axis(precip.MeltDegreeDay_USACE, 0, tavg2d, k=k, tbase=tbase)
 ppt_snow2d, ppt_rain2d = precip.PrecipPhase_2d(ppt2d, tavg2d, train, tsnow)
-
 swe_mod2d, act_melt2d = precip.ModelSWE_2d(ppt_snow2d, swe_melt2d)
-
 ppt_in2d = np.add(ppt_rain2d, act_melt2d)
+
+#calculate ET radiation
+Ra2d = rad.ExtraterrestrialRadiation_2d(np.full((nrow, ncol),conv.DegreesToRadians(41.97)), doy)
+
+#calculate PET
+pet2d = et.PET_Hargreaves1985(tmax2d, tmin2d, tavg2d, Ra2d)/1000.0 # m/day
+
+s = np.zeros(ppt_in2d.shape)
+r = np.zeros(ppt_in2d.shape)
+ra = np.zeros(ppt_in2d.shape) #accumulated runoff
+hwt = np.zeros(ppt_in2d.shape)
+qlat_out = np.zeros(ppt_in2d.shape)
+qlat_in = np.zeros(ppt_in2d.shape)
+perc = np.zeros(ppt_in2d.shape)
+sb = np.zeros(ppt_in2d.shape)
+bf = np.zeros(ppt_in2d.shape)
+q = np.zeros(ppt_in2d.shape)
+r[0,:,:] = 0 # set initial runoff m
+ra[0,:,:] = 0
+s[0,:,:] = 0.3 # set initial storage (i.e water content) m
+sb[0,:,:] = 0 # set initial aquifer storage (baseflow source) m
+soildepth = np.full((nrow, ncol), 1.0) #depth of soil profile m
+
+aet = np.zeros(ppt_in2d.shape)
+ksat = np.full((nrow, ncol), 1.0) # m/day
+slpds = gdal.Open(dirpath + "/testslopeper.tif")
+geot = slpds.GetGeoTransform()
+slope = slpds.GetRasterBand(1).ReadAsArray()
+# print("slope", slope)
+por = np.full((nrow, ncol), 0.5)
+fc = np.full((nrow, ncol), 0.3)
+wp = np.full((nrow, ncol), 0.1)
+ksub = np.full((nrow, ncol), 0.001) # m/day
+alpha = 0.02
+porl = np.multiply(por, soildepth)
+fcl = np.multiply(fc, soildepth)
+wpl = np.multiply(wp, soildepth)
+smax = porl
+qlat_nr = np.zeros(ndays)
+
+fprop = fr.FlowProportions(demnp)
+aet[0,:,:] = et.ET_theta_2d(pet2d[0,:,:], fcl, wpl, s[0,:,:])
+
+for i in range(1, ppt_in2d.shape[0]):
+    s[i, :, :] = s[i - 1, :, :] + ppt_in2d[i,:,:]
+    hwt[i, :, :] = gw.WaterTableHeight(por, fc, np.divide(s[i, :, :], soildepth), soildepth)
+    qlat_out[i, :, :] = gw.LateralFlow_Darcy_2d(ksat, slope, hwt[i, :, :], geot[1], geot[1])
+    qlat_in[i, :, :], qlat_nr[i] = fr.RouteFlow(fprop, qlat_out[i, :, :])
+    s[i, :, :] = s[i, :, :] + qlat_in[i,:,:] - qlat_out[i,:,:]
+    aet[i, :, :] = et.ET_theta_2d(pet2d[i, :, :], fcl, wpl, s[i - 1, :, :])
+    s[i,:,:] = s[i,:,:] - aet[i,:,:]
+    perc[i, :, :] = gw.Percolation_2d(ksub, gw.WaterTableHeight(por, fc, np.divide(s[i, :, :], 1.0), soildepth))
+    s[i, :, :] = s[i, :, :] - perc[i,:,:]
+    r[i, :, :] = np.where(s[i, :, :] > (soildepth*por), (s[i, :, :] - (soildepth*por)), 0.0)
+    ra[i, :, :] = rd.FlowAccumulation(rd.LoadGDAL(dempath), method='D8', weights=r[i,:,:])
+    # ra[i, :, :] = intm
+    s[i, :, :] = np.where(s[i, :, :] > (soildepth * por), (soildepth * por), s[i, :, :])
+
+# print("ppt", ppt_in2d)
+# print("pet", pet)
+# print("pet", pet2d)
+# print("et", et1)
+# print("et", et)
+# rd.FillDepressions(rddem, in_place=True)
+# accum = rd.FlowAccumulation(rddem, method="Dinf")
+# dinf_fig = rd.rdShow(accum, axes=False, cmap='jet')
+# accum = rd.FlowAccumulation(rddem, method="D8")
+# d8_fig = rd.rdShow(accum, axes=False, cmap='jet')
+# print("ppt", ppt_in2d)
+# print("hwt", hwt)
+# print("qlat out", qlat_out)
+# print("qlat in", qlat_in)
+# print("r", r)
+# print("s", s)
+plt.plot(date, qlat_in[:,4,2], 'g', date, ra[:,4,2], 'c', date, (r[:,4,2]+qlat_in[:,4,2]), 'b')
+plt.show()
+plt.plot(date, hwt[:,4,2], 'b')
+plt.show()
+plt.plot(date, s[:,4,2], 'b')
+plt.show()
 
